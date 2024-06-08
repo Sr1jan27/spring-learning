be1b3cd571be11ba5be59bd856587ede94eae468
srijan_singh <singhsrijan809@gmail.com>
2024-06-08 23:45:16 +0530

    uncomment

--- src/main/java/com/srijan/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/srijan/learn_spring_framework/App02HelloWorldSpring.java
@@ -30,9 +30,9 @@ public class App02HelloWorldSpring {
 		
 		System.out.println(context.getBean("person3Parameters"));
 //		
-//		System.out.println(context.getBean("address2"));
-//		
-//		System.out.println(context.getBean(Address.class));
+		System.out.println(context.getBean("address2"));
+		
+		System.out.println(context.getBean(Address.class));
 		
 		
 	}
