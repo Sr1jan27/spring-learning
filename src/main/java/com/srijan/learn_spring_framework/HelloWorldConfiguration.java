c72b7db15fc4b37bd4d25760fcb883dd942bc16d
srijan_singh <singhsrijan809@gmail.com>
2024-06-09 10:25:42 +0530

    lecture step 14

--- src/main/java/com/srijan/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/srijan/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.srijan.learn_spring_framework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
 // Released with Java JDK 16. 
@@ -40,9 +42,27 @@ public class HelloWorldConfiguration {
 		return new Person(name, age, address2);
 	}
 	
+	@Bean
+	@Primary
+	public Person person4Parameters(String name, int age, Address address) {
+		return new Person(name, age, address);
+	}
+	
+	@Bean
+	public Person person5Qualifier(String name, int age, @Qualifier("address3qulifier")Address address) {
+		return new Person(name, age, address);
+	}
 	
 	@Bean( name ="address2")
+	@Primary
 	public Address address() {
+		var address = new Address("R cube Properties", "Hyderabad");
+		return address;
+	}
+	
+	@Bean( name ="address3")
+	@Qualifier("address3qulifier")
+	public Address address3() {
 		var address = new Address("R cube Properties", "Hyderabad");
 		return address;
 	}
