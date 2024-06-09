499f18e8af2f849a6581bb1d47b3ff3e7dba7438
srijan_singh <singhsrijan809@gmail.com>
2024-06-09 14:03:44 +0530

    Component creation

--- src/main/java/com/srijan/learn_spring_framework/App03GamingSpringBeans.java
+++ src/main/java/com/srijan/learn_spring_framework/App03GamingSpringBeans.java
@@ -7,26 +7,10 @@ import org.springframework.context.annotation.Configuration;
 
 import com.srijan.learn_spring_framework.game.GameRunner;
 import com.srijan.learn_spring_framework.game.GamingConsole;
-import com.srijan.learn_spring_framework.game.MarioGame;
-import com.srijan.learn_spring_framework.game.PacmanGame;
-import com.srijan.learn_spring_framework.game.SuperContraGame;
-
 
 @Configuration
 @ComponentScan("com.srijan.learn_spring_framework.game")
 public class App03GamingSpringBeans {
-	
-//	@Bean
-//	public GamingConsole game() {
-//		var game = new PacmanGame();
-//		return game;
-//	}
-//	
-	@Bean
-	public GameRunner gameRunner(GamingConsole game) {
-		var gameRunner = new GameRunner(game);
-		return gameRunner;
-	}
 
 	public static void main(String[] args) {
 		
@@ -34,11 +18,7 @@ public class App03GamingSpringBeans {
 				new AnnotationConfigApplicationContext
 					(App03GamingSpringBeans.class)) {
 			
-//			System.out.println(context.getBean("game"));
-			
 			context.getBean(GamingConsole.class).left();
-			
-			System.out.println(context.getBean(GameRunner.class));
 			
 			context.getBean(GameRunner.class).run();
 			
