e55ea70264a50c49b64df8be49bb159187220210
srijan_singh <singhsrijan809@gmail.com>
2024-06-11 07:53:00 +0530

    Setter injection and constructor injection

--- src/main/java/com/srijan/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/srijan/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -2,7 +2,7 @@ package com.srijan.learn_spring_framework.examples.a1;
 
 import java.util.Arrays;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +11,37 @@ import org.springframework.stereotype.Component;
 @Component
 class YourBusinessClass {
 	
+	Dependency1 dependency1;
+	
+	Dependency2 dependency2;
+	
+	@Autowired
+	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+		super();
+		System.out.println("Constructor Injection -Your Business Class");
+		this.dependency1 = dependency1;
+		this.dependency2 = dependency2;
+	}
+
+//	
+//	@Autowired
+//	public void setDependency1(Dependency1 dependency1) {
+//		System.out.println("Setter Injection -Dep 1");
+//		this.dependency1 = dependency1;
+//	}
+//
+//
+//	@Autowired
+//	public void setDependency2(Dependency2 dependency2) {
+//		System.out.println("Setter Injection -Dep 2");
+//		this.dependency2 = dependency2;
+//	}
+
+
+
+	public String toString() {
+		return "Using " +  dependency1 + " and " + dependency2;
+	}
 }
 
 @Component
@@ -34,6 +65,8 @@ public class DepInjectionLauncherApplication {
 					(DepInjectionLauncherApplication.class)) {
 			
 			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			
+			System.out.println(context.getBean(YourBusinessClass.class));
 			
 		}
 
